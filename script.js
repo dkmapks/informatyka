6499b9668ffd2619df23e4296cf621386fafefba
dkmapks <dkmap039@gmail.com>
2025-04-10 08:59:11 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Prosty skrypt JavaScript
+document.getElementById("magicButton").addEventListener("click", function () {
+    const result = document.getElementById("result");
+    result.textContent = "Gratulacje, właśnie kliknąłeś magiczny przycisk!";
+    alert("Magiczny przycisk zadziałał!");
+});
